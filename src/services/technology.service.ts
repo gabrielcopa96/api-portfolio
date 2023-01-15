6586532e32e31503d3b9b58d80f7eac08ae95e23
Gabriel <gabrielcncopa@gmail.com>
2023-01-15 18:00:06 -0300

    initial feature technologies

---
+++ src/services/technology.service.ts
@@ -0,0 +1,27 @@
+
+
+const createTechnologies = async (body: any) => {
+   try {
+
+   } catch (error: any) {
+
+   }
+}
+
+const getAllTechnologies = async (body: any) => {
+   try {
+
+   } catch (error: any) {
+
+   }
+}
+
+const getOneTechnology = async (body: any, id: string) => {
+   try {
+
+   } catch (error: any) {
+
+   }
+}
+
+export { createTechnologies, getAllTechnologies, getOneTechnology }
